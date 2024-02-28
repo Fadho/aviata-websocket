d014fe09c45042b297da291c85d47c379662b43a
Fadipe Al-Ameen <fadipetomi00@gmail.com>
2024-02-28 15:06:04 +0100

    cxgfhf

--- src/modules/v1/busly-connection.ts
+++ src/modules/v1/busly-connection.ts
@@ -3,13 +3,13 @@ import { createReference } from "../common/utils";
 
 class RealTime {
     private reference = '';
-    private countdown = '10.0';
+    private countdown = '10';
     private countdownEndAt = 0
     private canPlaceBet = true;
     private readingOdds = '1.00'
-    private roundWaitTime = '5.0'
+    private roundWaitTime = '3.0'
     private generatedOdds = '0.00';
-    private oddsIncrement = 0.02
+    private oddsIncrement = 0.04
     private rangeOutcome = '';
     private date: Date | null = null;
     private roundWaitTimeEndAt = 0;
@@ -45,7 +45,7 @@ class RealTime {
         const hasWaitTimeEnded = Number(this.roundWaitTime) === this.roundWaitTimeEndAt;
 
         if (!hasWaitTimeEnded) {
-            this.roundWaitTime = Number(Number(this.roundWaitTime) - 0.2).toFixed(1);
+            this.roundWaitTime = Number(Number(this.roundWaitTime) - 0.4)>0 ? Number(Number(this.roundWaitTime) - 0.4).toFixed(1):'0';
         }
 
         if (hasWaitTimeEnded) {
@@ -55,18 +55,18 @@ class RealTime {
             }
 
             if (!hasCountDownEnded) {
-                this.countdown = Number(Number(this.countdown) - 0.2).toFixed(1);
+                this.countdown = String(Number(this.countdown) - 1);
             }
     
             if (hasCountDownEnded) {
                 this.canPlaceBet = false
                 this.readingOdds = Number(Number(this.readingOdds) + this.oddsIncrement).toFixed(2)
-                this.oddsIncrement += 0.002
+                this.oddsIncrement += 0.02
             }
     
             if (hasCountDownEnded && parseFloat(this.generatedOdds) <= parseFloat(this.readingOdds)) {
                 this.Clear()
-                this.roundWaitTime = Number(Number(this.countdown) + 1).toFixed(1);
+                this.roundWaitTime = Number(Number(this.roundWaitTime) ).toFixed(1);
             }
         }
 
@@ -88,12 +88,12 @@ class RealTime {
         this.canPlaceBet = false;
         this.generatedOdds = '0.00';
         this.rangeOutcome= '';
-        this.countdown = '10.0';
+        this.countdown = '10';
         this.reference = '';
         this.date = null;
         this.readingOdds = '1.00';
         this.oddsIncrement = 0
-        this.roundWaitTime = '0.0';
+        this.roundWaitTime = '3.0';
     }
 }
 
